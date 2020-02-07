29ffda66ec720a791387d5c35118027968ca7487
rpradhan <rpradhan@coredigital.com>
2020-02-06 16:48:23 -0800

    Adding Index

--- src/main/java/com/rpradhan/redis/application/model/Inquiry.java
+++ src/main/java/com/rpradhan/redis/application/model/Inquiry.java
@@ -1,7 +1,9 @@
 package com.rpradhan.redis.application.model;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
 
 import java.io.Serializable;
 
@@ -9,10 +11,13 @@ import java.io.Serializable;
  * @author rpradhan
  */
 @Data
-@RedisHash(value = "Inquiry", timeToLive = 60)
+@RedisHash(value = "Inquiry", timeToLive = 3600)
 public class Inquiry implements Serializable {
 
+    @Id
     private String id;
+
+    @Indexed
     private String marketingClickId;
     private String details;
 }
